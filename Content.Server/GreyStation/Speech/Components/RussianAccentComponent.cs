b4b5a6bb8088dc23db6e5d0af8531bcb9442eb20
ps3moira <113228053+ps3moira@users.noreply.github.com>
2024-01-28 02:03:36 -0800

    Added Russian Accent

---
+++ Content.Server/GreyStation/Speech/Components/RussianAccentComponent.cs
@@ -0,0 +1,8 @@
+using Content.Server.GreyStation.Speech.EntitySystems;
+
+namespace Content.Server.GreyStation.Speech.Components;
+
+[RegisterComponent]
+[Access(typeof(RussianAccentSystem))]
+public sealed partial class RussianAccentComponent : Component
+{ }
---
+++ Resources/Locale/en-US/greystation/traits/traits.ftl
@@ -0,0 +1,2 @@
+trait-russian-accent-name = Russian Accent
+trait-russian-accent-desc = A thick russian accent from the cold ice planets.
